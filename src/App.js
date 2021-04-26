527f3ecd96b688b84a5f8c727bdeddcf10692fac
Pavel <x9057557818x@gmail.com>
2021-04-26 22:20:58 +0300

    g

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {Main} from "./components/Main";
+import {AboutSchool} from "./components/AboutSchool";
+import {Study} from "./components/Study";
+import {Test} from "./components/Test";
+import {Authorization} from "./components/Authorization";
+import {ContactUs} from "./components/ContactUs";
+import {Footer} from "./components/Footer";
+import {Menu} from "./components/Menu";
+import {Route, BrowserRouter} from "react-router-dom";
+import {Registration} from "./components/Registration";
+import {Category} from "./components/Category";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+         <BrowserRouter>
+            <Menu/>
+            <Route exact path="/" render={()=><Main/>}/>
+            <Route path="/about" render={()=><AboutSchool/>}/>
+            <Route path="/admission" render={()=><Study/>}/>
+            <Route path="/why" render={()=><Test/>}/>
+            <Route path="/category" render={()=><Category/>}/>
+            <Route path="/contact" render={()=><ContactUs/>}/>
+            <Route path="/authorization" render={()=><Authorization/>}/>
+            <Route path="/registration" render={()=><Registration/>}/>
+            <Footer/>
+         </BrowserRouter>
+      </div>
   );
 }
 
